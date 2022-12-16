03b29d715df8a0a5f7f2864aa354531edf34e9ae
Michy <ezechimdindumichy@gmail.com>
2022-12-16 00:59:00 -0800

    update 1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet - print all the alphabet in lowercases
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchcar('\n');
+}
