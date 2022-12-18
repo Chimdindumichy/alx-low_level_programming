1ed831bfd2372c931b819ebe58106d60a6869f3d
Michy <ezechimdindumichy@gmail.com>
2022-12-17 19:10:37 -0800

    print numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - write a fun that print number
+ * @a: first number
+ * @: second nmber
+ * Return: always return num 0 and 9
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
