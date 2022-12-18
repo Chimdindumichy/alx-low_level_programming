427b46186fafdf73140bd48c585f84d0c4c76653
Michy <ezechimdindumichy@gmail.com>
2022-12-17 20:45:16 -0800

    üpdate 8 print sqare.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - prints a sqaure
+ * @size: square size
+ * return: nothing on success
+ */
+void print_square(int size)
+{
+	int count = 0, start;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (start = 0; start < size; start++)
+		{
+			while (count < size)
+			{
+				_putchar('#');
+				count++;
+			}
+		}
+	}
+}
