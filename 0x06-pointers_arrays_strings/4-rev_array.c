1cb0b9a675e4e1f79404d3f56376a11bb8161319
Michy <ezechimdindumichy@gmail.com>
2022-12-23 03:04:27 -0800

    update rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of array
+ * @a: the array of integer tobe reserved.
+ * @n: the num of elemet in the array.
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
