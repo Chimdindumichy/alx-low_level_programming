08dba910183dc20838b25c4e9836ce1e796c2fe5
Michy <ezechimdindumichy@gmail.com>
2022-12-27 00:35:06 -0800

    update 1st

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - data type
+ * @a: 1st pointer storing a value 5.
+ * @b: 2nd pointer storing a value 8.
+ * swap the valueof wo int.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int pr = 13;
+
+	a = &pr;
+	a = b;
+}
