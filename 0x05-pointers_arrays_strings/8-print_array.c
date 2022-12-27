12335cb2ff2d88bd89b6f095e77049b7702a4f88
Michy <ezechimdindumichy@gmail.com>
2022-12-27 04:01:39 -0800

    update 8 point array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print n element of an array of integers
+ * @a: point to array.
+ * @n: number of array elements.
+ *
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
